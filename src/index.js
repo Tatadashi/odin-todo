d720d12cb0d88d55d3253b99238f331ae192f8ba
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-03-27 01:50:09 -0700

    Add ToDo and Project class
    temp code to simulate class methods

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//non-dom js
+
+
+class ToDo {
+    //will create toDo from form data
+    constructor (title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+
+    changeProperty (property, newInfo) {
+        this[property] = newInfo;
+    }
+}
+
+class Project {
+    constructor (title, toDoList) {
+        this.title = title;
+        this.toDoList = toDoList;
+    }
+
+    changeProperty (property, newInfo) {
+        this[property] = newInfo;
+    }
+}
+
+const item1 = new ToDo('clean', 'cleaning', '12/02/2004', 'high', 'room');
+const item2 = new ToDo('hw', 'solving', '11/03/2013', 'low', 'math');
+
+item1.changeProperty('description', 'jon');
+
+const list = [item1, item2];
+
+const project = new Project('school', list);
+
+project.changeProperty('title', 'college');
+
+console.log(project);
