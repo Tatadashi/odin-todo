573957f5cc3b404f096910b6caacf8a28454991e
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-04-09 18:30:10 -0700

    Add about html
    currently temporary info inside

---
+++ src/otherJS/todos.js
@@ -0,0 +1,9 @@
+// import './todos.html';
+
+// function loadTodos () {
+//     $(document).ready(function() {
+//         $("#content").load('todos.html #chose');
+//      });
+// }
+
+// export {loadTodos};
