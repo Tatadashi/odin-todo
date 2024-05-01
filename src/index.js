770c645218106ddc5915bdfc0d1d21cc9c63fbf5
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2024-04-30 20:00:06 -0700

    Create way to update projects to sidebar visually
    Default is WIP. Todo content WIP

--- src/index.js
+++ src/index.js
@@ -4,41 +4,59 @@ import {setDOMEvents} from './otherJS/DOM';
 
 setDOMEvents();
 
-// class ToDo {
-//     //will create toDo from form data
-//     constructor (title, description, dueDate, priority, notes) {
-//         this.title = title;
-//         this.description = description;
-//         this.dueDate = dueDate;
-//         this.priority = priority;
-//         this.notes = notes;
-//     }
+function updateSidebar (projectList) {
+    //first child of #sidebar is where projects located
+    const sidebarProjects = document.getElementById('sidebar').children[0];
+    sidebarProjects.innerHTML = '';
+    
+    projectList.forEach(project => {
+        const projectDiv = document.createElement('div');
+        projectDiv.classList.add('project')
+        projectDiv.innerHTML = project.title;
+        // project.toDoList.forEach(todo => {
+        //     //code with content todo names
+        // });
+        sidebarProjects.appendChild(projectDiv);
+    });
+}
 
-//     changeProperty (property, newInfo) {
-//         this[property] = newInfo;
-//     }
-// }
+class ToDo {
+    //will create toDo from form data
+    constructor (title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
 
-// class Project {
-//     constructor (title, toDoList) {
-//         this.title = title;
-//         this.toDoList = toDoList;
-//     }
+    changeProperty (property, newInfo) {
+        this[property] = newInfo;
+    }
+}
 
-//     changeProperty (property, newInfo) {
-//         this[property] = newInfo;
-//     }
-// }
+class Project {
+    constructor (title, toDoList) {
+        this.title = title;
+        this.toDoList = toDoList;
+    }
 
-// const item1 = new ToDo('clean', 'cleaning', '12/02/2004', 'high', 'room');
-// const item2 = new ToDo('hw', 'solving', '11/03/2013', 'low', 'math');
+    changeProperty (property, newInfo) {
+        this[property] = newInfo;
+    }
+}
 
-// item1.changeProperty('description', 'jon');
+//default
+const item1 = new ToDo('clean', 'cleaning', '12/02/2004', 'high', 'room');
+const item2 = new ToDo('hw', 'solving', '11/03/2013', 'low', 'math');
+const todoList = [item1, item2];
+const project1 = new Project('school', todoList);
+const project2 = new Project('home', todoList);
+const projectList = [project1, project2];
 
-// const list = [item1, item2];
-
-// const project = new Project('school', list);
+updateSidebar(projectList);
 
 // project.changeProperty('title', 'college');
+// item1.changeProperty('description', 'jon');
 
 // console.log(project);
